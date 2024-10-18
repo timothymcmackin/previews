67baa945c7371a6d1ede080f78ca41a235a804bd
Tim McMackin <timothymcmackin@yahoo.com>
2024-10-18 10:47:40 -0400

    Remove old DAL PRs

--- octez/dal-modes/html/_static/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-   $('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
-});
-
-// Set light mode as the default
-(function () {
-   document.documentElement.dataset.mode = "light";
-})();
